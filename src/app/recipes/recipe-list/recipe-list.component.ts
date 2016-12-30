94240c35c8c81162c22f47e63a06e331e2a05287
Plastikaweb <info@plastikaweb.com>
2016-12-30 09:03:41 +0100

    dropdown directive and recipes class

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,8 +9,8 @@ export class RecipeListComponent implements OnInit {
     @Output() recipeSelected = new EventEmitter<Recipe>();
 
     recipes: Recipe[] = [
-        new Recipe( 'Dummy', 'Dummy', 'https://www.nutella.com/documents/8340659/8365575/nocciola.png/f393b2c8-2c86-4759-817b-ff6604864a16' ),
-        new Recipe( 'Kaa', 'Kaaaa', 'http://www.worldofzing.com/wp-content/uploads/2014/06/Chinese_White-chillli-120x120.jpg' ),
+        new Recipe( 'Dummy', 'Dummy', 'https://www.nutella.com/documents/8340659/8365575/nocciola.png/f393b2c8-2c86-4759-817b-ff6604864a16', [] ),
+        new Recipe( 'Kaa', 'Kaaaa', 'http://www.worldofzing.com/wp-content/uploads/2014/06/Chinese_White-chillli-120x120.jpg', [] ),
 
     ];
 
