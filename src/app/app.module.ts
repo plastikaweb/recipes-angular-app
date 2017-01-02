71474895465d01f5fbe7b7b5f1d08652183fb469
Plastikaweb <info@plastikaweb.com>
2017-01-02 20:49:24 +0100

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.component';
-import { DropdownDirective } from './dropdown.directive';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header.component';
+import {RecipesComponent} from './recipes/recipes.component';
+import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
+import {RecipeItemComponent} from './recipes/recipe-list/recipe-item.component';
+import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {ShoppingListAddComponent} from './shopping-list/shopping-list-add.component';
+import {DropdownDirective} from './dropdown.directive';
 import {RecipeService} from './recipes/recipe.service';
+import {ShoppingListService} from './shopping-list/shopping-list.service';
+import {mainRouting} from './app.routing';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import {RecipeStartComponent} from './recipes/recipe-start.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
-    ShoppingListComponent,
-    ShoppingListAddComponent,
-    DropdownDirective
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [RecipeService],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+@NgModule( {
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeItemComponent,
+        RecipeDetailComponent,
+        ShoppingListComponent,
+        ShoppingListAddComponent,
+        DropdownDirective,
+        RecipeEditComponent,
+        RecipeStartComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        mainRouting
+    ],
+    providers: [
+        RecipeService,
+        ShoppingListService
+    ],
+    bootstrap: [AppComponent]
+} )
+export class AppModule {
+}
---
+++ src/app/app.routing.ts
@@ -0,0 +1,20 @@
+import {Routes, RouterModule} from '@angular/router';
+import { ModuleWithProviders} from '@angular/core';
+import {RecipesComponent} from './recipes/recipes.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {RecipeStartComponent} from './recipes/recipe-start.component';
+import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
+import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
+
+const APP_ROUTES: Routes = [
+    {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+    {path: 'recipes', component: RecipesComponent, children: [
+        {path: '', component: RecipeStartComponent},
+        {path: 'new', component: RecipeEditComponent},
+        {path: ':id', component: RecipeDetailComponent},
+        {path: ':id/edit', component: RecipeEditComponent}
+    ]},
+    {path: 'shopping-list', component: ShoppingListComponent}
+];
+
+export const mainRouting: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
