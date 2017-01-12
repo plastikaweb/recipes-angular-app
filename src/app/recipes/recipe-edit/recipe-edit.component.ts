80673efe2dc37dc3812e02c294d1a1d0d14dac03
Plastikaweb <info@plastikaweb.com>
2017-01-12 15:27:15 +0100

    recipe edit and cancel

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {RecipeService} from '../recipe.service';
 import {Subscription} from 'rxjs/Subscription';
 import {Recipe} from '../recipe';
@@ -18,7 +18,8 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
 
     constructor(private route: ActivatedRoute,
                 private recipeService: RecipeService,
-                private formBuilder: FormBuilder) {
+                private formBuilder: FormBuilder,
+                private router: Router) {
     }
 
     ngOnInit() {
@@ -70,6 +71,24 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
             ingredients: recipeIngredients
         } )
 
+    }
+
+    private navigateBack() {
+        this.router.navigate(['../']);
+    }
+
+    onSubmit() {
+        const newRecipe = this.recipeForm.value;
+        if (this.isNew) {
+            this.recipeService.addRecipe( newRecipe );
+        } else {
+            this.recipeService.editRecipe( this.recipe, newRecipe );
+        }
+        this.navigateBack();
+    }
+
+    onCancel() {
+        this.navigateBack();
     }
 
 }
