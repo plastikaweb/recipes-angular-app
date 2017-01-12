3babb96476c2c28a4c313797b13a01c975b50691
Plastikaweb <info@plastikaweb.com>
2017-01-12 15:42:33 +0100

    edit add or remove ingredients

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -74,7 +74,7 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
     }
 
     private navigateBack() {
-        this.router.navigate(['../']);
+        this.router.navigate( ['../'] );
     }
 
     onSubmit() {
@@ -89,6 +89,22 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
 
     onCancel() {
         this.navigateBack();
+    }
+
+    onAddItem(name: string, amount: string) {
+        (<FormArray>this.recipeForm.controls['ingredients']).push(
+          new FormGroup({
+              name: new FormControl( name, Validators.required ),
+              amount: new FormControl( amount, [
+                  Validators.required,
+                  Validators.pattern( "\\d+" )
+              ] )
+          })
+        );
+    }
+
+    onRemoveItem(index: number) {
+        (<FormArray>this.recipeForm.controls['ingredients']).removeAt(index);
     }
 
 }
